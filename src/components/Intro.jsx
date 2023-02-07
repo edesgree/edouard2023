6b861faacf8b3732d612988736032cc7dc22b235
Edouard Desgrée <edouard.desgree@gmail.com>
2023-02-07 20:45:25 +0100

    intro icons and css

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import PhotoEd from '../assets/images/home-edouard.png';
+import ItemGreen from '../assets/images/item-green.svg';
+import ItemMustard from '../assets/images/item-mustard.svg';
+import ItemSage from '../assets/images/item-sage.svg';
 export default function Intro() {
   return (
     <div>
       <section className="columns">
-        <div className="column">
-          <span className="sup-title">Front-end developer | web designer</span>
-          <h1>Hi 👋, I'm Edouard.</h1>
+        <div className="column is-two-thirds">
+          <h5 className="subtitle is-5 sup-title">
+            Front-end developer | web designer
+          </h5>
+
+          <h1 className="title is-1 ">
+            Hi 👋,
+            <br /> I'm Edouard.
+          </h1>
           <p>
             I am a frontend developer with a strong background in UX/UI. I try
             to make things simple and modern using best practices and newest
@@ -13,29 +23,29 @@ export default function Intro() {
             it be in web development or my personal interests.
           </p>
         </div>
-        <div className="column">
-          <img src={`../assets/home-edouard.png`} alt="Edouard Desgrée" />
+        <div className="column is-one-thirds">
+          <img src={PhotoEd} width="205" alt="Edouard Desgrée" />
         </div>
       </section>
 
       <section className="level">
         <div class="level-item has-text-centered">
           <div>
-            <img src={`../assets/item-green.png`} alt="" />
+            <img src={ItemGreen} alt="" />
             <p class="heading">Tweets</p>
             <p class="title">3,456</p>
           </div>
         </div>
         <div class="level-item has-text-centered">
           <div>
-            <img src={`../assets/item-green.png`} alt="" />
+            <img src={ItemSage} alt="" />
             <p class="heading">Tweets</p>
             <p class="title">3,456</p>
           </div>
         </div>
         <div class="level-item has-text-centered">
           <div>
-            <img src={`../assets/item-green.png`} alt="" />
+            <img src={ItemMustard} alt="" />
             <p class="heading">Tweets</p>
             <p class="title">3,456</p>
           </div>

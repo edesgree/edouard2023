0c2e0774f8549f2a62d158e24886bda753feb7de
Edouard Desgrée <edouard.desgree@gmail.com>
2023-02-10 21:44:21 +0100

    svg animation on contact page

---
+++ src/components/Contact.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+export default function Contact(props) {
+  return (
+    <>
+      <section class="hero">
+        <div class="hero-body">
+          <h1 className="title has-text-centered is-1 pre-line ">
+            {props.dataText.contactTitle[props.lang]}
+          </h1>
+          <svg
+            className="textContact"
+            viewBox="0 0 960 100"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <symbol id="s-text">
+              <text text-anchor="middle" x="50%" y="80%">
+                edouard@desgree.com
+              </text>
+            </symbol>
+
+            <g class="g-ants">
+              <use xlinkHref="#s-text" class="letter"></use>
+              <use xlinkHref="#s-text" class="letter"></use>
+              <use xlinkHref="#s-text" class="letter"></use>
+              <use xlinkHref="#s-text" class="letter"></use>
+              <use xlinkHref="#s-text" class="letter"></use>
+            </g>
+          </svg>
+        </div>
+      </section>
+    </>
+  );
+}

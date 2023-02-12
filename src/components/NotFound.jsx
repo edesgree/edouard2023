cc9a820431aed9b21923c4d7d40b032ec5169204
Edouard Desgrée <edouard.desgree@gmail.com>
2023-02-12 22:24:36 +0100

    404 not found

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Gif404 from '../assets/images/404.gif';
+
 export default function NotFound() {
   return (
-    <section>
+    <section className="section has-text-centered">
       <h2 className="is-2 title">Not found</h2>
+      <img src={Gif404} alt="404 not found" />
     </section>
   );
 }

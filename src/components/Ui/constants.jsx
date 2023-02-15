bb99f9e706f5ed9b233c0d614258a8f5f0b164e2
Edouard Desgrée <edouard.desgree@gmail.com>
2023-02-15 18:56:49 +0100

    test svg anim

--- src/components/Ui/constants.jsx
+++ src/components/Ui/constants.jsx
@@ -12,8 +12,18 @@ const animPop = {
   visible: { opacity: 1, scale: 4, transitionSpring },
   hidden: { opacity: 0, scale: 0 }
 };
-const squareVariants = {
-  visible: { opacity: 1, scale: 4, transition: { duration: 1 } },
-  hidden: { opacity: 0, scale: 0 }
+const animDraw = {
+  hidden: { pathLength: 0, opacity: 0 },
+  visible: (i) => {
+    const delay = 1 + i * 0.5;
+    return {
+      pathLength: 1,
+      opacity: 1,
+      transition: {
+        pathLength: { delay, type: 'spring', duration: 1.5, bounce: 0 },
+        opacity: { delay, duration: 0.01 }
+      }
+    };
+  }
 };
-export { animPop, transitionSpring, animShowContentVariant };
+export { animPop, transitionSpring, animShowContentVariant, animDraw };

d02124077d0d612fb3accebe7712a880ed74d819
Edouard Desgrée <edouard.desgree@gmail.com>
2023-02-15 16:00:09 +0100

    anim constants

--- src/components/Ui/constants.jsx
+++ src/components/Ui/constants.jsx
@@ -8,4 +8,12 @@ const animShowContentVariant = {
   visible: { opacity: 1, transition: { duration: 0.8 } },
   hidden: { opacity: 0 }
 };
-export { transitionSpring, animShowContentVariant };
+const animPop = {
+  visible: { opacity: 1, scale: 4, transitionSpring },
+  hidden: { opacity: 0, scale: 0 }
+};
+const squareVariants = {
+  visible: { opacity: 1, scale: 4, transition: { duration: 1 } },
+  hidden: { opacity: 0, scale: 0 }
+};
+export { animPop, transitionSpring, animShowContentVariant };

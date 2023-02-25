4688d1ff42b55b6ed355d5f649384444978f99c0
Edouard Desgrée <edouard.desgree@gmail.com>
2023-02-25 22:31:35 +0100

    lazy load des components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import React, { Fragment } from 'react';
-import { Route, Routes, useLocation, useRoutes } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import data from './assets/data';
 import Header from './components/Header';
 import Intro from './components/Intro';
-import Work from './components/Work';
-import WorkDetail from './components/WorkDetail';
-import Lab from './components/Lab';
-import LabDetail from './components/LabDetail';
-import Test from './components/Test';
 import About from './components/About';
 import Contact from './components/Contact';
 import Skills from './components/Skills';
 import NotFound from './components/NotFound';
 import Layout from './components/Ui/Layout';
-import InfiniteLooper from './components/InfiniteLooper';
 import ScrollUpButton from './components/Ui/ScrollUpButton';
 import { animShowContentVariant } from './components/Ui/constants';
-import {
-  motion,
-  useAnimation,
-  AnimatePresence,
-  useIsPresent
-} from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
+const Work = lazy(() => import('./components/Work'));
+const WorkDetail = lazy(() => import('./components/WorkDetail'));
+const Lab = lazy(() => import('./components/Lab'));
+const LabDetail = lazy(() => import('./components/LabDetail'));
+
+function Loading() {
+  return <h2>Loading...⌛</h2>;
+}
 function App() {
   const [lang, setLang] = React.useState('fr');
 
@@ -68,13 +65,15 @@ function App() {
           <Route
             path="/work/*"
             element={
-              <Work
-                dataWork={data.work}
-                dataText={data.common}
-                lang={lang}
-                handleTrad={handleTrad}
-                anim={animShowContentVariant}
-              />
+              <Suspense fallback={<Loading />}>
+                <Work
+                  dataWork={data.work}
+                  dataText={data.common}
+                  lang={lang}
+                  handleTrad={handleTrad}
+                  anim={animShowContentVariant}
+                />
+              </Suspense>
             }
           />
 
@@ -82,41 +81,47 @@ function App() {
             path="work/:projectName"
             element={
               <>
-                <AnimatePresence mode="wait" initial={false}>
-                  <WorkDetail
-                    dataWorkDetail={data.work}
-                    dataText={data.common}
-                    lang={lang}
-                    anim={animShowContentVariant}
-                  />
-                </AnimatePresence>
+                <Suspense fallback={<Loading />}>
+                  <AnimatePresence mode="wait" initial={false}>
+                    <WorkDetail
+                      dataWorkDetail={data.work}
+                      dataText={data.common}
+                      lang={lang}
+                      anim={animShowContentVariant}
+                    />
+                  </AnimatePresence>
+                </Suspense>
               </>
             }
           />
           <Route
             path="/lab"
             element={
-              <Lab
-                dataLab={data.lab}
-                dataText={data.common}
-                lang={lang}
-                handleTrad={handleTrad}
-                anim={animShowContentVariant}
-              />
+              <Suspense fallback={<Loading />}>
+                <Lab
+                  dataLab={data.lab}
+                  dataText={data.common}
+                  lang={lang}
+                  handleTrad={handleTrad}
+                  anim={animShowContentVariant}
+                />
+              </Suspense>
             }
           />
           <Route
             path="lab/:projectName"
             element={
               <>
-                <AnimatePresence mode="wait" initial={false}>
-                  <LabDetail
-                    dataLabDetail={data.lab}
-                    dataText={data.common}
-                    lang={lang}
-                    anim={animShowContentVariant}
-                  />
-                </AnimatePresence>
+                <Suspense fallback={<Loading />}>
+                  <AnimatePresence mode="wait" initial={false}>
+                    <LabDetail
+                      dataLabDetail={data.lab}
+                      dataText={data.common}
+                      lang={lang}
+                      anim={animShowContentVariant}
+                    />
+                  </AnimatePresence>
+                </Suspense>
               </>
             }
           />

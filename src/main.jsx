b0278f41f080676f691a1cce0fb6441ff3101351
Edouard Desgrée <edouard.desgree@gmail.com>
2023-02-25 12:54:47 +0100

    add google analytics tracker

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './assets/styles.scss';
+import TagManager from 'react-gtm-module';
+
+// google analytics
+const tagManagerArgs = {
+  gtmId: 'G-9RS14XS58K'
+};
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

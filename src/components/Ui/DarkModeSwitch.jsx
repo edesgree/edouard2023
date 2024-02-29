20d44a17aa75a9ceefcb9a629e6186ef83b2cc69
Edouard Desgrée <edouard.desgree@gmail.com>
2024-02-29 18:08:38 +0100

    add useeffect on darkmode

--- src/components/Ui/DarkModeSwitch.jsx
+++ src/components/Ui/DarkModeSwitch.jsx
@@ -1,53 +1,60 @@
 import { motion } from 'framer-motion';
-import React, { useState } from 'react';
-import IconSun from './icons/IconSun';
-import IconMoon from './icons/IconMoon';
+import React, { useEffect } from 'react';
 import { transitionSpring } from './constants';
+import IconMoon from './icons/IconMoon';
+import IconSun from './icons/IconSun';
 
 export default function DarkModeSwitch() {
-  const [darkMode, setDarkMode] = React.useState(() => {
-    if (localStorage.getItem('theme') === 'light') {
-      return true;
-    } else {
-      return false;
-    }
-  });
+    const [darkMode, setDarkMode] = React.useState(() => {
+        return localStorage.getItem('theme') === 'light';
+    });
 
-  const toggleSwitch = () => setDarkMode(!darkMode);
+    const toggleSwitch = () => setDarkMode(prevDarkMode => !prevDarkMode);
 
-  if (darkMode) {
-    document.body.classList.remove('darkMode');
-    document.body.classList.add('lightMode');
-    localStorage.setItem('theme', 'light');
-  } else {
-    document.body.classList.add('darkMode');
-    document.body.classList.remove('lightMode');
-    localStorage.setItem('theme', 'dark');
-  }
+    useEffect(() => {
+        if (darkMode) {
+            document.body.classList.remove('darkMode');
+            document.body.classList.add('lightMode');
+            localStorage.setItem('theme', 'light');
+        } else {
+            document.body.classList.add('darkMode');
+            document.body.classList.remove('lightMode');
+            localStorage.setItem('theme', 'dark');
+        }
+    }, [darkMode]);
 
-  if (
-    localStorage.theme === 'light' ||
-    (!('theme' in localStorage) &&
-      window.matchMedia('(prefers-color-scheme: light)').matches)
-  ) {
-    document.body.classList.add('darkMode');
-    document.body.classList.remove('lightMode');
-  } else {
-    document.body.classList.add('lightMode');
-    document.body.classList.remove('darkMode');
-  }
-
-  return (
-    <button
-      onClick={toggleSwitch}
-      className="button is-secondary is-small"
-      aria-label="switch theme color"
-    >
-      <span className="icon is-small">
-        <motion.div className="icon-svg" transition={transitionSpring}>
-          {darkMode ? <IconSun /> : <IconMoon />}
-        </motion.div>
-      </span>
-    </button>
-  );
+    useEffect(() => {
+        const prefersLightMode = window.matchMedia('(prefers-color-scheme: light)').matches;
+        if (localStorage.getItem('theme') === 'light' || (!localStorage.getItem('theme') && prefersLightMode)) {
+            setDarkMode(false);
+        } else {
+            setDarkMode(true);
+        }
+    }, []);
+    /* 
+      if (
+        localStorage.theme === 'light' ||
+        (!('theme' in localStorage) &&
+          window.matchMedia('(prefers-color-scheme: light)').matches)
+      ) {
+        document.body.classList.add('darkMode');
+        document.body.classList.remove('lightMode');
+      } else {
+        document.body.classList.add('lightMode');
+        document.body.classList.remove('darkMode');
+      }
+    */
+    return (
+        <button
+            onClick={toggleSwitch}
+            className="button is-secondary is-small"
+            aria-label="switch theme color"
+        >
+            <span className="icon is-small">
+                <motion.div className="icon-svg" transition={transitionSpring}>
+                    {darkMode ? <IconSun /> : <IconMoon />}
+                </motion.div>
+            </span>
+        </button>
+    );
 }

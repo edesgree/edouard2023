1fb1729e2e5e38aedca21c1819802d2a38545a77
Edouard Desgrée <edouard.desgree@gmail.com>
2024-02-29 18:09:50 +0100

    cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import TagManager from 'react-gtm-module';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './assets/styles.scss';
-import TagManager from 'react-gtm-module';
 
 // google analytics
 const tagManagerArgs = {
-  gtmId: 'G-9RS14XS58K'
+    gtmId: 'G-9RS14XS58K'
 };
+
 TagManager.initialize(tagManagerArgs);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );

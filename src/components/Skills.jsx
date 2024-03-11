80a82d18583e2216fb7b4c1299ff08cd2f6e3667
Edouard Desgrée <edouard.desgree@gmail.com>
2024-03-11 19:40:30 +0100

    new skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 export default function Skills(props) {
-  const skillsElements = props.dataSkills.map((item) => {
-    return (
-      <li key={item.id} id={item.id} data-tooltip={item.name}>
-        <img src={`${item.icon}`} alt={item.name} />
-      </li>
-    );
-  });
+    const skillsElements = props.dataSkills.map((item) => {
+        return (
+            <li key={item.slug} data-tooltip={item.name}>
+                <img src={`${item.icon}`} alt={item.name} />
+            </li>
+        );
+    });
 
-  return (
-    <section className="section">
-      <h2 className="is-3 title">{props.dataText.skillsTitle[props.lang]}</h2>
-      <ul className="skills-list">{skillsElements}</ul>
-    </section>
-  );
+    return (
+        <section className="section">
+            <h2 className="is-3 title">{props.dataText.skillsTitle[props.lang]}</h2>
+            <ul className="skills-list">{skillsElements}</ul>
+        </section>
+    );
 }
